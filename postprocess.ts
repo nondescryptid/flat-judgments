b46545e0a12161470fb9b88ce8a805f8d92a80e9
nondescryptid <suzukitomoe.st@gmail.com>
2023-02-08 23:57:39 +0800

    manual push bc my postprocessing script is buggy

--- postprocess.ts
+++ postprocess.ts
@@ -12,30 +12,30 @@ try {
         writeJSON('data.json', parsed_feed)
     } 
     // check if data.json exists, then start parsing links
-    if('data.json'){
-        const data = await readJSON('data.json')
-        // console.log(data.entries)
-        await Promise.all(data.entries.map(async (item) => {
- // Get links
-        const response = await fetch(item.id);
-        const response_html = await response.text();
-        // Trims everything after the last '-' - thanks OGP
-        const blacklistChars = /[^A-Za-z0-9-()_+&@,\s]/g
-        const itemTitle = item.title.value.split('-')
-        let fileName = itemTitle.pop().trim().replace(/ /g, '_')
-        fileName = fileName.replace(blacklistChars, '')
 
-        // Check if file already exists
-        try {
-            const file = await Deno.open(`./cases/${fileName}.html`)
-            Deno.close(file.rid)
-        } catch(e) {
-            if (e instanceof Deno.errors.NotFound) {
-                writeTXT(`./cases/${fileName}.html`, response_html)
-            }
+    const data = await readJSON('data.json')
+    // console.log(data.entries)
+    await Promise.all(data.entries.map(async (item: { id: string|URL|Request; title: { value: string; }; }) => {
+// Get links
+    const response = await fetch(item.id);
+    const response_html = await response.text();
+    // Trims everything after the last '-' - thanks OGP
+    const blacklistChars = /[^A-Za-z0-9-()_+&@,\s]/g
+    const itemTitle: string[] = item.title.value.split('-')
+    let fileName = itemTitle.pop().trim().replace(/ /g, '_')
+    fileName = fileName.replace(blacklistChars, '')
+
+    // Check if file already exists
+    try {
+        const file = await Deno.open(`./cases/${fileName}.html`)
+        Deno.close(file.rid)
+    } catch(e) {
+        if (e instanceof Deno.errors.NotFound) {
+            writeTXT(`./cases/${fileName}.html`, response_html)
         }
-        }))
     }
+    }))
+    
 } catch (error) {
     console.log(error)
 }
